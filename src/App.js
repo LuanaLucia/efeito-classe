ed849fe3e7b17eac3d9408c8f92c875b3ee49965
LuanaLucia <luanasantos@outlook.com>
2023-05-06 10:04:10 -0300

    roteamento

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import Card from './Card';
+import Imagem from './Imagem';
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      <Router>
+          <ul>
+            <li>
+              <Link to="/">Girar</Link>
+            </li>
+            <li>
+              <Link to="/imagem">Imagem</Link>
+            </li>
+          </ul>
+          <hr />
+          <Routes>
+            <Route path='/' element={<Card />} />
+            <Route path='/imagem' element={<Imagem />} />
+          </Routes>
+
+      </Router>
+
     </div>
   );
 }
---
+++ src/Imagem.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+function Imagem(){
+    return(
+        <img src="https://apod.nasa.gov/apod/image/2305/TwilightFlower1024.jpg" alt="" />
+    );
+}
+export default Imagem;
